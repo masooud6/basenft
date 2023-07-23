5dec7fc2fa238ee374a365ce4f728a8e10c3f1a2
0xmasoud <102187885+masooud6@users.noreply.github.com>
2023-07-23 16:38:42 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xc3439615111e4e486a493ea77d9f06fcf2269c09";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
